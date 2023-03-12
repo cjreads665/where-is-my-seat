c81d64acbac6fc62dbdcca4a592d5969d29e8534
cjreads665 <shahid.alam57@gmail.com>
2023-03-12 19:35:30 +0545

    added toast

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,15 @@ import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import Footer from "./Footer"
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
   return (
     <div className="App">
+   <ToastContainer
+position="top-center"
+/>
       <Header />
       <Home />
       <Footer/>
--- src/Home/PnrForm.jsx
+++ src/Home/PnrForm.jsx
@@ -1,10 +1,11 @@
 import axios from 'axios';
 import React, { useState } from 'react'
-
+import { toast } from 'react-toastify';
 const PnrForm = () => {
   const [pnr,setPnr] = useState()
   const pattern = /^[0-9]*$/;
   // console.log(pattern.test(888));
+
   return (
 <form action="">
     <label htmlFor="">Enter PNR Number</label>
@@ -17,12 +18,19 @@ const PnrForm = () => {
     <button className='common-btn' 
     onClick={(e)=>{
       e.preventDefault();
+      const id = toast.loading("Please wait...")
       axios.post(`${import.meta.env.VITE_BACKEND_URL}/get-status`,{
         pnr
       })
-      .then(res=>console.log(res))
+      .then(res=>{
+        toast.update(id, {render: "Successfully fetched status", type: "success", isLoading: false,autoClose: 2000});
+      })
+      .catch(e=>{
+        toast.update(id, {render: "Something went wrong", type: "error", isLoading: false,autoClose: 2000 });
+      })
     }}
     >Get PNR Status</button>
+            
   </form>
   )
 }
